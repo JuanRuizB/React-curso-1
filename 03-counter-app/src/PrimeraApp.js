d3827168d82e26ae621c115a1d663ecaa1c32f20
JuanRuizB <jrb300@live.com>
2021-02-18 19:27:12 +0100

    Tercera sección

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const PrimeraApp = () => {
+
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
+
+}
+
+
+export default PrimeraApp
